830e94e61115fc4e22265830291f2e19bf2e4b74
yyyyjinying <1020066395@@qq.com>
2020-12-13 19:37:08 +0800

    antd配置

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -93,6 +93,14 @@ let config = {
                 ['@babel/plugin-transform-runtime'],
                 // ['@babel/proposal-class-properties'],
                 ['@babel/proposal-object-rest-spread'],
+                [
+                  'import',
+                  {
+                    libraryName: 'antd',
+                    pg: 'es',
+                    style: true,
+                  },
+                ],
               ],
             },
           },
--- src/common/components/index.tsx
+++ src/common/components/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import {Button} from "antd";
 
 class Index extends React.Component {
     render(){
-        return <div>common</div>
+        return <div>
+            <Button type="primary" icon="download" size={"large"} />
+            <Button>antd button</Button>
+        </div>
     } 
 }
 
