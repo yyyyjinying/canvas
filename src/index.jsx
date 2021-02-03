570eb43345858b72a5c94a8ec8e0985b427c662d
yyyyjinying <1020066395@@qq.com>
2021-02-03 16:26:53 +0800

    all

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import '../public/rem';
 // import Comm from '@common/components';
 import Canvas from './page/canvas';
 import { Button } from 'yyyyjinying-button-test';
+import ZButton from 'z-jinying-test';
 import './style.less';
 // import "./tapable";
 // import Car from "./car.jpg";
@@ -19,32 +20,33 @@ import './style.less';
 const sub = React.createElement('h1', { id: 'zhao', title: '一个子节点' }, '标题文本01');
 // eslint-disable-next-line no-unused-vars
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            num: 0,
-        };
-    }
-    emitFn(params, num) {
-        console.log('emitfn', num, params);
-        this.setState((state) => {
-            return { num: ++state.num };
-        });
-    }
-    render() {
-        console.log('用户代理: ' + navigator.userAgent);
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: 0,
+    };
+  }
+  emitFn(params, num) {
+    console.log('emitfn', num, params);
+    this.setState((state) => {
+      return { num: ++state.num };
+    });
+  }
+  render() {
+    console.log('用户代理: ' + navigator.userAgent);
 
-        return (
-            <div>
-                {/* iphone */}
-                {/* <Comm /> */}
-                <Button color="blur">按钮</Button>
-                <Canvas />
-                {/* {sub} */}
-                {/* <window.other.default num={this.state.num} emitFn={this.emitFn.bind(this)} /> */}
-            </div>
-        );
-    }
+    return (
+      <div>
+        {/* iphone */}
+        {/* <Comm /> */}
+        <z-button />
+        <z-button color="blur">按钮</z-button>
+        <Canvas />
+        {/* {sub} */}
+        {/* <window.other.default num={this.state.num} emitFn={this.emitFn.bind(this)} /> */}
+      </div>
+    );
+  }
 }
 
 render(<App />, document.getElementById('app'));
