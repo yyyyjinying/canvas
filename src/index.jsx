0a80dfefbe1dd656e96004f5b8bb1ab18c9ac847
yyyyjinying <1020066395@@qq.com>
2021-02-05 09:14:43 +0800

    rootdir

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,3 @@
-// import { Button } from 'yyyyjinying-button-test';
-// class Index extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <Button color="blur">按钮</Button>
-//         <span>zhaojinying</span>
-//       </div>
-//     );
-//   }
-// }
-
 import ZForm from './components/z-form';
 
 export { ZForm };
