44f6fa56bf7c88cfe5fb62ea2bdb91f6bb344ae2
yyyyjinying <1020066395@@qq.com>
2021-02-05 10:40:33 +0800

    bug

--- src/page/form/index.jsx
+++ src/page/form/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Button } from 'antd';
 import { Form } from 'r-form-antd';
 import { columnRefs } from './columnRefs';
 class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      params: {
+        select: 'bb',
+        num: 232,
+      },
+    };
+  }
   render() {
     return (
-      <Form // className="z-form"
-        // // mapPropsToFields={this.mapPropsToFields.bind(this)}
-        // // onValuesChange={this.onValuesChange.bind(this)}
-        columns={columnRefs.call(this)}
-      />
+      <div>
+        <Button>fdf</Button>
+        <Form
+          className="z-form"
+          // // mapPropsToFields={this.mapPropsToFields.bind(this)}
+          // // onValuesChange={this.onValuesChange.bind(this)}
+          columns={columnRefs.call(this)}
+        />
+      </div>
     );
   }
 }
